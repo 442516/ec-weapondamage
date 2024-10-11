5a4e070aef26b6f5192ee52637da981f86dae9e1
EddieCarson <89758706+442516@users.noreply.github.com>
2024-10-11 11:22:29 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,73 @@
+Config = {}
+
+
+Config.Weapons = {
+    {
+        weapon_name = 'WEAPON_DAGGER',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_BAT',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_DAWEAPON_BOTTLEGGER',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_CROWBAR',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_UNARMED',
+        weapon_damage = 0.2,
+    },
+    {
+        weapon_name = 'WEAPON_FLASHLIGHT',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_GOLFCLUB',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_HAMMER',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_HATCHET',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_KNIFE',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_MACHETE',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_SWITCHBLADE',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_NIGHTSTICK',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_WRENCH',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_BATTLEAXE',
+        weapon_damage = 0.13,
+    },
+    {
+        weapon_name = 'WEAPON_POOLCUE',
+        weapon_damage = 0.1,
+    },
+    {
+        weapon_name = 'WEAPON_STONE_HATCHET',
+        weapon_damage = 0.13,
+    },
+}
